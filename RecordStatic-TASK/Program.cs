88d84130aae6a80dd283513f9543e910594996ea
Akbar Jafarli <akbaraj@code.edu.az>
2024-06-12 14:47:33 +0400

    Add project files.

---
+++ RecordStatic-TASK/Program.cs
@@ -0,0 +1,62 @@
+﻿using RecordStatic_TASK;
+
+User[] users = new User[3];
+for (int i = 0; i < users.Length; i++)
+{
+    Console.WriteLine($"Enter details for user  {i + 1}:");
+    Console.Write("Fullname:");
+    string fullname = Console.ReadLine();
+    Console.Write("Email:");
+    string email = Console.ReadLine();
+    Console.Write("Password:");
+    string password = Console.ReadLine();
+    Console.WriteLine(" ");
+
+    users[i] = new User(fullname, email, password);
+}
+
+while (true)
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1)Show all students.");
+    Console.WriteLine("2)Get info by Id.");
+    Console.WriteLine("3)Quit.");
+    Console.WriteLine(" ");
+    Console.WriteLine("Select:");
+    int choice = int.Parse(Console.ReadLine());
+    if (choice == 1)
+    {
+        Console.WriteLine("All students:");
+        foreach (User user in users)
+        {
+            user.GetInfo();
+        }
+        //Console.ReadLine();
+        //Console.Clear();
+    }
+    else if (choice == 2)
+    {
+        Console.WriteLine("Enter user Id");
+        int id = int.Parse(Console.ReadLine());
+        User founduser = User.FindUserById(id, users);
+        if (founduser != null)
+        {
+            Console.WriteLine("User found:");
+            founduser.GetInfo();
+        }
+        else
+        {
+            Console.WriteLine("User not found:");
+        }
+        //Console.ReadLine() ;
+        //Console.Clear() ;
+    }
+    else if (choice == 3)
+    {
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Please enter correct choice:");
+    }
+}
---
+++ RecordStatic-TASK/User.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecordStatic_TASK
+{
+    class User
+    {
+        private static int _id;
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string fullName, string email, string password)
+        {
+            Id = _id++;
+            FullName = fullName;
+            Email = email;
+            Password = password;
+        }
+
+        public bool PasswordChecker()
+        {
+            bool IsUpperLetter = false;
+            bool IsLowerLetter = false;
+            bool IsDigit = false;
+
+            if (Password.Length < 8)
+            {
+                return false;
+            }
+            foreach (char c in Password)
+            {
+                if (char.IsUpper(c))
+                {
+                    IsUpperLetter = true;
+                }
+                if (char.IsLower(c))
+                {
+                    IsLowerLetter = true;
+                }
+                if (char.IsDigit(c))
+                {
+                    IsDigit = true;
+                }
+            }
+            return IsUpperLetter && IsLowerLetter && IsDigit;
+
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id: {Id} - Fullname: {FullName} - Email: {Email}");
+        }
+
+        public static User FindUserById(int id, User[] users)
+        {
+            foreach (User user in users)
+            {
+                if (user.Id == id)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
